2713048c62e02289c0e980990ce9621161c9fc83
Sindre Sørensen <>
2021-04-21 19:40:17 +0200

    Trying to get my user id from getUserId, but no bueno so far.

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,21 +1,33 @@
 import useSWR from "swr";
 import StateSWR from "components/Layout/StateSWR";
 import { useGlobalState } from "components/StateManagement/GlobalStateProvider";
+import { CodeSharp } from "@material-ui/icons";
+import { getUserId } from "../lib/mongoUtils";
+import React, { useState, useEffect } from "react";
+
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 
 const Profile = () => {
+  const [userMe, setUserMe] = useState(0);
+  const authCode = localStorage.getItem("authCode") || "";
+  //useEffect(() => { setUserMe( getUserId(authCode)) });
 
+  // const getter = async (authCode) => {
+  //  return await getUserId(authCode);
+  // }
 
-  const { state } = useGlobalState();
+  //console.log(authCode);
+  //const me = getUserId(authCode);
+
   const userId = 11385085;
   const { data, error } = useSWR(`/api/results/user/${11385085}`, fetcher);
 
   if (!data) return <StateSWR />;
   if (error) return <StateSWR error={true} />;
 
-  console.log(data);
+  //console.log(data);
 
   return (
     <div className="mb-5 text-center h-72 bg-red-100">
